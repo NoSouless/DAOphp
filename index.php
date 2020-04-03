3d62467d4a42d6917dba3f4df241b9baa3813936
unknown <leo.caselato@gmail.com>
2020-04-03 17:59:32 -0300

    metodos de insert

--- index.php
+++ index.php
@@ -3,42 +3,36 @@
 require_once("config.php");
 
 /*$sql = new Sql();
-
 $usuarios = $sql->select(
     "SELECT 
         * 
     FROM 
         tb_usuarios
 ");
-
 echo json_encode($usuarios);*/
 
 /*Carrega um usuário
-
 $root = new Usuario();
-
 $root->loadbyID(15);
+echo $root;*/
 
-echo $root;
-
-Carrega uma lista de usuários
-
+/*Carrega uma lista de usuários
 $lista = Usuario::getList();
+echo json_encode($lista);*/
 
-echo json_encode($lista);
-
-carrega uma lista de usuários buscando pelo login
-
+/*carrega uma lista de usuários buscando pelo login
 $search = Usuario::search("jo");
+echo json_encode($search);*/
 
-echo json_encode($search);
-
-carrega um usuario usando o login e a senha*/
-
+/*carrega um usuario usando o login e a senha
 $usuario = new Usuario();
-
 $usuario->login("root", "3333");
+echo $usuario;*/
 
-echo $usuario;
+$aluno = new Usuario("aluno", "@lun0");
+
+$aluno->insert();
+
+echo $aluno;
 
 ?>
