704aea1e6739530e46e24c1c51119d96c040f9c7
unknown <leo.caselato@gmail.com>
2020-04-03 17:10:13 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -13,10 +13,32 @@ $usuarios = $sql->select(
 
 echo json_encode($usuarios);*/
 
+/*Carrega um usuário
+
 $root = new Usuario();
 
 $root->loadbyID(15);
 
 echo $root;
+
+Carrega uma lista de usuários
+
+$lista = Usuario::getList();
+
+echo json_encode($lista);
+
+carrega uma lista de usuários buscando pelo login
+
+$search = Usuario::search("jo");
+
+echo json_encode($search);
+
+carrega um usuario usando o login e a senha*/
+
+$usuario = new Usuario();
+
+$usuario->login("root", "3333");
+
+echo $usuario;
 
 ?>
